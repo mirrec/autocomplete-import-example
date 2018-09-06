a2224915f4b41794319948eb5d60a275cf8e6e5b
Miroslav Hettes <mirrec@gmail.com>
2018-09-06 18:05:49 +0200

    Fix typos

--- lib/weby.ex
+++ lib/weby.ex
@@ -5,6 +5,8 @@ defmodule Weby do
   def init(_), do: []
 
   def call(conn, _opts) do
+    # LOCAL MODULE
+    #
     # Utils.       auto-complete works
     # coun         does not work. it should suggest counter from imported module Utils
 
@@ -12,8 +14,10 @@ defmodule Weby do
     counter()
 
 
-    # Plug.Conn.p  auto-complete works
-    # send_re      does not work. it should suggest counter from imported module Utils
+    # DEPENDENCY
+    #
+    # Plug.Conn.s  auto-complete works
+    # send_re      does not work. it should suggest `send_resp` from imported module `Plug.Conn`
 
     # CMD+click suggest that function is from Plug.Conn module
     send_resp(conn, 200, "Hello world")
